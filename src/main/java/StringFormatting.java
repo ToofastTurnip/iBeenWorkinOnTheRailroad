c0ca42bb0206092064cc166cbc002de739eb5d1d
Mitch Taylor <taylor.mitch.5@gmail.com>
2018-03-13 17:01:54 -0400

    String format practice

--- src/main/java/StringFormatting.java
+++ src/main/java/StringFormatting.java
@@ -1,7 +1,29 @@
+import java.util.ArrayList;
+
 public class StringFormatting {
 
     public static void main(String[] args) {
+
         // Ready to roll baby
+
+        String testStringOne = "Text";
+        String testNumberOne = "1";
+        String testStringTwo = "Long Text";
+        String testNumberTwo = "22";
+        String testStringTre = "T";
+        String testNumberTre = "3";
+
+        ArrayList<String> strings = new ArrayList<String>();
+        strings.add(testStringOne);
+        strings.add(testNumberOne);
+        strings.add(testStringTwo);
+        strings.add(testNumberTwo);
+        strings.add(testStringTre);
+        strings.add(testNumberTre);
+
+        for (String string : strings) {
+            System.out.format("%32s", string);
+        }
     }
 
 }
