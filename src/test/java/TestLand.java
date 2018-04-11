72562ff2ada1ae81e66ec55d8c20b6ee97ee5614
Mitch Taylor <taylor.mitch.5@gmail.com>
2018-04-11 08:37:52 -0400

    May have broken TestLand

--- src/test/java/TestLand.java
+++ src/test/java/TestLand.java
@@ -8,4 +8,9 @@ public class TestLand {
         String originalString = "naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##naME:BreaD;price:1.23;type:Food;expiration:1/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:2/25/2016##naMe:MiLK;price:3.23;type:Food^expiration:1/11/2016##naMe:Cookies;price:2.25;type:Food%expiration:1/25/2016##naMe:CoOkieS;price:2.25;type:Food*expiration:1/25/2016##naMe:COokIes;price:2.25;type:Food;expiration:3/22/2016##naMe:COOkieS;price:2.25;type:Food;expiration:1/25/2016";
     }
 
+    @Test
+    public void fatFingerTest1() {
+
+    }
+
 }
